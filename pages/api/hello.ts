4f64ac3d69a81a9ae735688fad61665d4e44b87b
ahmadrosid <alahmadrosid@gmail.com>
2023-01-13 17:27:41 +0700

    feat: rate limit next.js

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import redis from "../../lib/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+import requestIp from "request-ip";
 
-type Data = {
-  name: string
-}
+const LIMIT_PER_SECOND = 3;
+const ratelimit = redis
+  ? new Ratelimit({
+    redis: redis,
+    limiter: Ratelimit.fixedWindow(LIMIT_PER_SECOND, "60 s"),
+  })
+  : undefined;
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
+  console.log({ratelimit});
+  
+  if (ratelimit) {
+    const identifier = requestIp.getClientIp(req);
+    const result = await ratelimit.limit(identifier!);
+    res.setHeader("X-RateLimit-Limit", result.limit);
+    res.setHeader("X-RateLimit-Remaining", result.remaining);
+
+    if (!result.success) {
+      res
+        .status(429)
+        .json(
+          "Too many uploads in 1 minute. Please try again in a few minutes."
+        );
+      return;
+    }
+  }
+
   res.status(200).json({ name: 'John Doe' })
 }
---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl --request GET \
+  --url http://localhost:3001/api/hello
+
+curl --request GET \
+  --url http://localhost:3001/api/hello
+
+curl --request GET \
+  --url http://localhost:3001/api/hello
+
+curl --request GET \
+  --url http://localhost:3001/api/hello
